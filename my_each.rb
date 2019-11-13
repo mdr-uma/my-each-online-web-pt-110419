7177e1fa6383807e04c5cced4f77789414baadb2
Uma Manandhar <um_2003_8@hotmail.com>
2019-11-13 18:11:32 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,8 @@
 def my_each(array) # put argument(s) here
-  while
-    
-  end# code here
+  counter = 0
+  while counter < array.length
+    yield (array[counter])
+    counter += 1
+
+    end# code here
 end
